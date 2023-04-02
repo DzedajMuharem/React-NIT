114da001eaf1ae0adc335509da7da7e197cb059b
DzedajMuharem <mujanovich100@gmail.com>
2023-04-02 15:32:55 +0200

    Change

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Greeting from "./components/Greetings/Greeting";
 
 function App() {
   // return React.createElement("p", {}, "Neki tekst");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <> je Fragment
+    <>
+      <div className="App">
+        <header className="header">
+          <h1 style={{ color: "red", fontFamily: "sans-serif" }}>Logo</h1>
+        </header>
+        <Greeting appName={"React App"} username={"Bakir Ujkanovic"} />
+      </div>
+    </>
   );
 }
 
